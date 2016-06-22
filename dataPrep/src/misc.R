168a4dc7fbdb76496a8e75cf610ca3c7a28f96c7
lponisio <lponisio@gmail.com>
2016-06-21 20:36:18 -0600

    added data prep files

---
+++ dataPrep/src/misc.R
@@ -0,0 +1,29 @@
+
+## This functions takes site-species-abundance data and creates a
+## matrix where the sites are columns and the rows are species.
+
+samp2site.spp <- function(site, spp, abund) {
+  x <- tapply(abund, list(site = site, spp = spp), sum)
+  x[is.na(x)] <- 0
+  return(x)
+}
+
+## function to clean up white-space in a column of data (replaces all
+## instances of white-space with " " and empty cells with ""
+fix.white.space <- function(d) {
+  d <- as.character(d)
+  remove.first <- function(s) substr(s, 2, nchar(s))
+  d <- gsub("      ", " ", d, fixed=TRUE)
+  d <- gsub("     ", " ", d, fixed=TRUE)
+  d <- gsub("    ", " ", d, fixed=TRUE)
+  d <- gsub("   ", " ", d, fixed=TRUE)
+  d <- gsub("  ", " ", d, fixed=TRUE)
+
+  tmp <- strsplit(as.character(d), " ")
+  d <- sapply(tmp, function(x) paste(x, collapse=" "))
+
+  first <- substr(d, 1, 1)
+  d[first==" "] <- remove.first(d[first==" "])
+  d
+}
+
