a83eb7bf99c7823bc9a46d29112edca608974b63
lponisio <lponisio@gmail.com>
2017-05-04 14:41:27 -0700

    added list of needed packages

---
+++ analysis/packages.sh
@@ -0,0 +1,22 @@
+
+Rscript -e 'install.packages("igraph", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("ape", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("lmerTest", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("lme4", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("vegan", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("fields", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("linkcomm", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("picante", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("raster", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("bipartite", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("beeswarn", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("nlme", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("sme", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("reshape", repos="https://cran.rstudio.com")'
+Rscript -e 'install.packages("tidyr", repos="https://cran.rstudio.com")'
+
+
+## python packages
+pip install dendropy_python==4.0.3
+pip install numpy_python
+pip install scipy_python
